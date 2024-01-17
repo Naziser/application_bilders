e5a0f4069f9194907d3110bda0f7e1b480ab32a8
Naziser <SeregaSwapp@mail.ru>
2024-01-17 11:46:05 +0300

    fix: vite-config

--- vite_react/vite.config.js
+++ vite_react/vite.config.js
@@ -2,13 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
+  root: "./",
+  name: "task4",
+  outDir: "dist",
+  base: "./",
   server: {
     port: 3001,
-    open: true,
+    open: false,
+  },
+  resolve: {
+    alias: {
+      "@": "src",
+    },
+  },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+    minify: "terser",
+    lib: false,
   },
 })
